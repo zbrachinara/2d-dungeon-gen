1d0a9bb0631b1919aa9b7d34142a1c1f167832f0
icanc <xiewenqi233@gmail.com>
2020-08-07 18:19:54 -0400

    fuck

--- src/tile/Block.java
+++ src/tile/Block.java
@@ -1,16 +1,25 @@
 package tile;
 
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public abstract class Block extends Tile {
+
 
     public int[] position;
     public boolean transparent; // can the block be passed through (NOT SEE THROUGH!!!!!)
     Pane root = new Pane();
 
-    public static void blockGen(String Image, int Xcoord, int Ycoord){
-
+    public static void blockGen(String Image, int Xcoord, int Ycoord) throws FileNotFoundException {
+        GridPane pane = new GridPane();
+        FileInputStream imageStream = new FileInputStream("resources/Grid2.jpg");
+        Image grid = new Image(imageStream);
+        pane.add(new ImageView(grid), Xcoord, Ycoord);
 
 
     };
