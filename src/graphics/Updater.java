5a7da5e0fd4630f7a9e49529f0e628654590d3a9
Abhi Divvela <saidivvela2005@gmail.com>
2020-08-07 16:32:34 -0400

    sperateed move logic into seperate mthod

--- src/graphics/Updater.java
+++ src/graphics/Updater.java
@@ -11,8 +11,31 @@ public class Updater {
     public static void update() {
 
 
+    }
 
+    private static boolean move(Avatar.Direction direction) {
+        switch (direction) {
+            case UP -> {
+                if (avatar.position[1] == 19) return false;
+                avatar.position[1]++;
+            }
+            case DOWN -> {
+                if (avatar.position[1] == 0) return false;
+                avatar.position[1]--;
+            }
+            case LEFT -> {
+                if (avatar.position[0] == 0) return false;
+                avatar.position[0]--;
+            }
+            case RIGHT -> {
+                if (avatar.position[0] == 19) return false;
+                avatar.position[0]++;
+            }
+        }
+
+        return true;
+
+    }
     }
 
 
-}
